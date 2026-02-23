4e7caabe41048be98b56e1d7c6463e5c8e9bbc9e
Akash Madisetty <144154768+akashmadisetty@users.noreply.github.com>
2026-02-23 15:23:23 +0530

    UPDATED the code

--- backend/app.cpp
+++ backend/app.cpp
@@ -39,17 +39,19 @@ int main() {
     
     // Accept connections in loop
     while(true) {
-        int client_fd = accept(server_fd, NULL, NULL);
-        if (client_fd < 0) continue;
-        
-        // Simple HTTP response
-        std::string response = "HTTP/1.1 200 OK\r\n";
-        response += "Content-Type: text/plain\r\n";
-        response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
-        
-        send(client_fd, response.c_str(), response.length(), 0);
-        close(client_fd);
+    int client_fd = accept(server_fd, NULL, NULL);
+    if (client_fd < 0) continue;
+
+    char buffer[1024] = {0};
+    read(client_fd, buffer, sizeof(buffer));  // <-- READ request first
+
+    std::string response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-Type: text/plain\r\n";
+    response += "Connection: close\r\n\r\n";
+    response += "Served by backend: " + std::string(hostname) + "\n";
+
+    send(client_fd, response.c_str(), response.length(), 0);
+    close(client_fd);
     }
     
     return 0;
